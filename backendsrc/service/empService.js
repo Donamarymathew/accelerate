0480f25f31f3b51951bc3d7410aa5b632d27e0a6
Donamarymathew <donamarymathew97@gmail.com>
2020-04-30 14:45:01 +0530

    Update empService.js

--- backendsrc/service/empService.js
+++ backendsrc/service/empService.js
@@ -1,6 +1,7 @@
 const empdb=require('../model/empModel');
 let employeeSerive={}
 const validator=require('../utilities/validator');
+const bcrypt=require('bcrypt');
 
 //get details of all employees
 employeeSerive.getAllEmployees=()=>{
@@ -35,18 +36,23 @@ employeeSerive.getEmployeeById = ((employeeId) => {
 
 //verify empId and password
 employeeSerive.loginEmployee=(staffId,password)=>{
-    return empdb.loginEmployee(staffId,password)
-    .then((response)=>{
-        if(response==null){
-            let err=new Error("Wrong Credential")
-            err.status=404;
-            throw err
-        }
-        else {
-            return {"message":"Login Successful "};
-        }
-    })
+    return empdb.findEmployee(staffId).then(async(emp)=>
+    {if(emp==null)
+        { let err = new Error("Employee not Registered");
+        err.status = 404;
+        throw err;}
+    else{
+       if( await bcrypt.compare(password,emp.password)){
+         return {"message":"Login Successful "};
+       }
+       else{
+        let err=new Error("Wrong Credential")
+        err.status=404;
+        throw err
+       }
     }
+    })
+}
 
 
 //register employee
@@ -54,22 +60,29 @@ employeeSerive.registerEmployee=((empObj)=>{
     console.log(empObj.staffId);
     validator.validateempId(empObj.staffId);
     validator.validatepassword(empObj.password);
-    return empdb.findEmployee(empObj.staffId).then(object=>{
-        if(object!=null){
-            let err= new Error("Employee already registered with the employeeID ");
-            err.status=404;
-            throw err;
-        }else{
-            return empdb.registerEmployee(empObj).then((data)=>{
-                if(data){
-                return {"message": "Successfully registered employee with ID: "+data}
-            }
-                else{
-                    let err= new Error("Employee Details not Inserted");
-                    err.status=500;
-                    throw err;
+    console.log(1);
+        return empdb.findEmployee(empObj.staffId).then(async object=>{
+            if(object!=null){
+                let err= new Error("Employee already registered with the employeeID ");
+                err.status=404;
+                throw err;
+            }else{
+                const salt=await bcrypt.genSalt();
+                const hashpassword=await bcrypt.hash(empObj.password,salt)
+                empObj.password=hashpassword;
+                return empdb.registerEmployee(empObj).then((data)=>{
+                    if(data){
+                        console.log("yes");
+                    return {"message": "Successfully registered employee with ID: "+data}
                 }
-            })}})})
+                    else{
+                        let err= new Error("Employee Details not Inserted");
+                        err.status=500;
+                        throw err;
+                    }
+                })}})
+    
+ })
 
 //delete employee
 employeeSerive.deleteEmployee=(empId)=>{
@@ -97,13 +110,16 @@ employeeSerive.deleteEmployee=(empId)=>{
 
 //update password
 employeeSerive.updateEmployee=((employeeId,password)=>{
-    return empdb.findEmployee(employeeId).then(object=>{
+    return empdb.findEmployee(employeeId).then(async object=>{
         if(object==null){
             let err= new Error("Employee not  registered!!");
             err.status=404;
             throw err;
         }else{
     validator.validatepassword(password);
+    const salt=await bcrypt.genSalt();
+    const hashpassword=await bcrypt.hash(password,salt)
+    password=hashpassword;
     return empdb.updateEmployee(employeeId,password).then(data=>{
         if(data){
             return {"message": "Successfully updated password of employee with ID: "+data}
